feaffe5f6643acb78de2bd55d166851882a396e5
Vidal-Gus <vidal47pro@gmail.com>
2023-10-10 16:26:21 -0300

    adicionei a funcao de adicionar produto e calcular total

--- src/controladores/compras.js
+++ src/controladores/compras.js
@@ -10,6 +10,40 @@ const operacoes = {
             console.log(e);
             return res.status(500).json({ mensagem: "Erro no servidor!!" })
         }
+    },
+    adicionarCompra: async function (req, res) {
+        const { nome, preco } = req.body;
+        if (!nome || !preco) {
+            return res.status(400).json({ mensagem: "Nome e preco são obrigatórios!" })
+        }
+        try {
+            const query = `
+            insert into lista_compras (nome, preco) values ($1, $2) returning *;
+            `
+            const resultado = await mercado.query(query, [nome, preco]);
+
+            return res.status(201).json(resultado.rows);
+        } catch (e) {
+            console.log(e);
+            return res.status(500).json({ mensagem: "Erro no servidor!" })
+        }
+    },
+    calcularTotal: async function (req, res) {
+        try {
+            const resultado = await mercado.query(`select sum(preco) from lista_compras;`);
+
+            if (resultado.rowCount < 1) {
+                return res.status(404).json({ mensagem: "Itens não encontrados!" })
+            }
+
+            const valorBruto = resultado.rows[0].sum;
+            const valorFormatado = valorBruto / 100;
+
+            return res.status(200).json({ valorBruto, valorFormatado });
+        } catch (e) {
+            console.log(e);
+            return res.status(500).json({ mensagem: "Erro no servidor!" })
+        }
     }
 }
 
--- src/rotas.js
+++ src/rotas.js
@@ -4,5 +4,7 @@ const compras = require('./controladores/compras');
 const rota = express();
 
 rota.get('/', compras.listarCompras);
+rota.post('/adicionar', compras.adicionarCompra);
+rota.get('/total', compras.calcularTotal);
 
 module.exports = rota
